b773448bb8464e38e8e658d4991f93ad0aa7fba2
Nursultan301 <abykeevnursultan14@gmail.com>
2022-05-31 22:04:11 +0600

    Created models.py

--- medical/models.py
+++ medical/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
-# Create your models here.
+
+class Doctors(models.Model):
+    """Врачи"""
+    name = models.CharField("Имя", max_length=50)
+    description = models.TextField("Описание")
+    img = models.ImageField(upload_to='doctors/')
+
+    class Meta:
+        verbose_name = "Врач"
+        verbose_name_plural = "Врачи"
+
+    def __str__(self):
+        return self.name
+
+
+class Enroll(models.Model):
+    WEEK = (
+        ("sun", "Понедельник"),
+        ("mon", "Вторник"),
+        ("tue", "Среда"),
+        ("wed", "Четверг"),
+        ("thu", "Пятница"),
+    )
+
+    name = models.CharField("Имя", max_length=50)
+    email = models.EmailField("E-mail")
+    weeks = models.CharField("Недели", max_length=50, choices=WEEK, default="sun")
+    time = models.TimeField("Время")
+    doctors = models.ForeignKey(Doctors, on_delete=models.CASCADE, verbose_name="Врачи")
+    messages = models.TextField("Сообщения")
+
+    class Meta:
+        verbose_name = "Запись на прием"
+        verbose_name_plural = "Запись на прием"
+
+    def __str__(self):
+        return self.name
+
+
+class Feedback(models.Model):
+    name = models.CharField("Имя", max_length=50)
+    email = models.EmailField("E-mail")
+    telephone = models.CharField("Телефон", max_length=50)
+    requirements = models.CharField("Требования", max_length=50)
+    messages = models.TextField("Сообщения")
+
+    class Meta:
+        verbose_name = "Обратная связь"
+        verbose_name_plural = "Обратная связь"
+
+    def __str__(self):
+        return self.name
+
+
+class Subscription(models.Model):
+    email = models.EmailField("E-mail")
+    date = models.DateTimeField("Дата")
+
+    class Meta:
+        verbose_name = "Подписка"
+        verbose_name_plural = "Подписки"
+
+    def __str__(self):
+        pass
